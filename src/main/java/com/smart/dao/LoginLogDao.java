db338ae909425d857cf07e9984b923e18a8e71c4
pengrenjun <809507790@qq.com>
2018-03-12 12:01:45 +0800

    Initial commit

---
+++ src/main/java/com/smart/dao/LoginLogDao.java
@@ -0,0 +1,30 @@
+package com.smart.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.smart.domain.LoginLog;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+
+public class LoginLogDao {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	//保存登陆日志SQL
+	private final static String INSERT_LOGIN_LOG_SQL= "INSERT INTO t_login_log(user_id,ip,login_datetime) VALUES(?,?,?)";
+
+	public void insertLoginLog(LoginLog loginLog) {
+		Object[] args = { loginLog.getUserId(), loginLog.getIp(),
+				          loginLog.getLoginDate() };
+		int b=jdbcTemplate.update(INSERT_LOGIN_LOG_SQL, args);
+
+	}
+
+	@Autowired
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+}
---
+++ src/main/java/com/smart/service/UserService.java
@@ -0,0 +1,49 @@
+package com.smart.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import com.smart.dao.LoginLogDao;
+import com.smart.dao.UserDao;
+import com.smart.domain.LoginLog;
+import com.smart.domain.User;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+//这个注解将UserService标注为服务层的bean
+@Service
+
+public class UserService {
+	@Autowired
+	private UserDao userDao;
+	@Autowired
+	private LoginLogDao loginLogDao;
+	public boolean hasMatchUser(String userName, String password) {
+		int matchCount =userDao.getMatchCount(userName, password);
+		return matchCount > 0;
+	}
+	
+	public User findUserByUserName(String userName) {
+		return userDao.findUserByUserName(userName);
+	}
+    //@Transactional
+    public void loginSuccess(User user) {
+		user.setCredits( 5 + user.getCredits());
+		LoginLog loginLog = new LoginLog();
+		loginLog.setUserId(user.getUserId());
+		loginLog.setIp(user.getLastIp());
+		loginLog.setLoginDate(user.getLastVisit());
+
+		try {
+			userDao.updateLoginInfo(user);
+			loginLogDao.insertLoginLog(loginLog);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+	}
+
+
+}
