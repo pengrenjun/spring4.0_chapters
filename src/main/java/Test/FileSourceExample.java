2b9d37078b189c172d31ae57d4b36b71517bea79
pengrenjun <809507790@qq.com>
2018-03-13 14:56:45 +0800

    spring Resource接口对资源访问的多种读写方法

--- src/main/java/Test/FileSourceExample.java
+++ src/main/java/Test/FileSourceExample.java
@@ -1,12 +1,16 @@
 
 package Test;
 
+import com.deloitte.si.core.utils.FileUtils;
+import com.smart.domain.XhContractExecution;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.PathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.WritableResource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.util.FileCopyUtils;
 
-import java.io.IOException;
+import java.io.*;
 
 /*通过spring的Resource抽象接口的实现类访问文件资源*/
 public class FileSourceExample {
@@ -18,21 +22,92 @@ public class FileSourceExample {
 
         //使用系统绝对路径的方式访问资源
         String filePath="E:\\下载的资料\\精通spring4.x工程源码\\chapter2\\src\\main\\java\\com\\smart\\domain\\XhContractExecution.java".replace("\\","/");
-        WritableResource writableResource= getPathResource(filePath);
+        WritableResource writableResource= (WritableResource) getPathResource(filePath);
         System.out.println(writableResource.isWritable());
 
         //使用类路径加载文件
-        String classResoucePath="resources/smart-contect.xml";
+        String classResoucePath="main/resources/smart-context.xml";
         Resource classResource=getClassResouce(classResoucePath);
-        System.out.println();
 
+        File file= getFileByResoure(classResource);
+        System.out.println("文件的系统绝对路径： "+file.getAbsolutePath());
+
+        System.out.println(getFileRootPath(XhContractExecution.class));
+
+        String path="E:\\下载的资料\\精通spring4.x工程源码\\chapter2\\src\\conf\\asd.text".replace("\\","/");
+        WritableResourceTest(path);
+        System.out.println("读取的文件内容："+ReadResourceTest(path));
+
+
+        /*通过EmcodeResource对资源文件进行编码处理*/
+        Resource resourceToEncode=getPathResource(path);
+
+        String content= getFileContent(resourceToEncode,"UTF-8");
+        System.out.println("读取的文件内容utf-8: "+content);
+
+    }
+
+    /*获取特殊编码风格的文件内容信息(通过EncodeResource对Resource进行资源编码处理)*/
+    private static String getFileContent(Resource resourceToEncode,String codeStyle) throws IOException {
+       return  FileCopyUtils.copyToString(getEncodeResource(resourceToEncode,codeStyle).getReader());
+    }
+
+    /*通过Resource获取特殊编码的EncodeResource*/
+    private static EncodedResource getEncodeResource(Resource resourceToEncode,String codeStyle) {
+        return  new EncodedResource(resourceToEncode,codeStyle);
+    }
+
+    /*通过resource获取File对象*/
+    private static File getFileByResoure(Resource resource) throws IOException {
+        return resource.getFile();
+    }
+
+    /*通过Resource接口读取文件资源*/
+    private static String ReadResourceTest(String path) throws IOException {
+        Resource resource=getPathResource(path);
+        InputStream inputStream=resource.getInputStream();
+        //ByteArrayOutputStream他的作用就是把字节流转成字符
+        ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+        int i;
+        while ((i=inputStream.read())!=-1){
+            System.out.print(i+" ");
+            byteArrayOutputStream.write(i);
+        }
+        inputStream.close();
+        byteArrayOutputStream.close();
+
+        return byteArrayOutputStream.toString();
+
+    }
+
+    //使用WritableResource接口向系统所在文件中写资源(filePath为系统绝对路径)
+     private static void WritableResourceTest(String filePath) throws IOException {
+        WritableResource writableResource=(WritableResource)getPathResource(filePath);
+
+         OutputStream outputStream=  getOutputStream(writableResource);
+         //这个写法回覆盖之前文件中的内容
+         outputStream.write("资源文件写入测试".getBytes());
+         outputStream.flush();
+    }
+
+
+    /*通过WritableResource获取相应的输出流FileOutputStream*/
+    private static OutputStream getOutputStream(WritableResource writableResource) throws IOException {
+        //writableResource完成写资源操作,实际调用的是jdk的outputStream
+        return  writableResource.getOutputStream();
     }
 
     private static Resource getClassResouce(String str) {
         return  new ClassPathResource(str);
     }
 
-    private  static WritableResource getPathResource(String str){
+    private  static Resource getPathResource(String str){
         return new PathResource(str);
     }
+    //获取类加载的根路径(系统跟路径)
+    private static String getFileRootPath(Class<?> cls){
+        return  cls.getClass().getResource("/").getPath();
+    }
+
+
 }
