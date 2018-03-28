ea30855c20895daef9b4e8d89582812fb8b79388
pengrenjun <35021872+pengrenjun@users.noreply.github.com>
2018-03-28 20:00:08 +0800

    容器级的国际化信息资源的配置

--- src/main/java/Test/ResourceBundelTest.java
+++ src/main/java/Test/ResourceBundelTest.java
@@ -14,48 +14,46 @@ public class ResourceBundelTest {
 
     public static void main(String[] args) throws InterruptedException {
 
-        ResourceBundle cnRes=ResourceBundle.getBundle("resource", Locale.CHINESE);
+        ResourceBundle cnRes = ResourceBundle.getBundle("resource", Locale.CHINESE);
         System.out.println(cnRes.getString("one"));
 
-        ResourceBundle usRes=ResourceBundle.getBundle("resource", Locale.US);
+        ResourceBundle usRes = ResourceBundle.getBundle("resource", Locale.US);
         System.out.println(usRes.getString("one"));
 
         /*动态配置资源文件信息*/
 
         //加载区域资源文件 cnRes usRes
         //用本地化格式配置区域资源文件
-        Object params[]={new Date()};
-        String cnStr=new MessageFormat(cnRes.getString("one"),Locale.CHINA).format(params);
+        Object params[] = {new Date()};
+        String cnStr = new MessageFormat(cnRes.getString("one"), Locale.CHINA).format(params);
         System.out.println(cnStr);
 
-        String enStr=new MessageFormat(cnRes.getString("one"),Locale.US).format(params);
+        String enStr = new MessageFormat(cnRes.getString("one"), Locale.US).format(params);
         System.out.println(enStr);
 
         /*通过配置MessageSource的实现类ResourceBundleMessageSource,批量加载不同区域资源文件信息*/
         //简化了上面的资源加载方式
-        ApplicationContext ctx=new ClassPathXmlApplicationContext(new String[]{"beans.xml"});
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(new String[]{"beans.xml"});
 
         //获取MessageSource的配置Bean
-        MessageSource messageSource= (MessageSource) ctx.getBean("resourceBean");
+        MessageSource messageSource = (MessageSource) ctx.getBean("resourceBean");
 
-        String cnMessage=messageSource.getMessage("one",params,Locale.CHINA);
-        String enMessage=messageSource.getMessage("one",params,Locale.US);
-        System.out.println("通过MessageSource加载文件信息"+cnMessage);
-        System.out.println("通过MessageSource加载文件信息"+enMessage);
+        String cnMessage = messageSource.getMessage("one", params, Locale.CHINA);
+        String enMessage = messageSource.getMessage("one", params, Locale.US);
+        System.out.println("通过MessageSource加载文件信息" + cnMessage);
+        System.out.println("通过MessageSource加载文件信息" + enMessage);
 
         //定时刷新资源配置测试
-        MessageSource reMessage=(MessageSource)ctx.getBean("reloadSourceBean");
-        String aEnMessage=reMessage.getMessage("one",params,Locale.US);
-        System.out.println("刷新前的信息"+aEnMessage);
-        Thread.sleep(20000);
-        String bEnMessage=reMessage.getMessage("one",params,Locale.US);
-        System.out.println("刷新后的信息"+bEnMessage);
+        /*MessageSource reMessage = (MessageSource) ctx.getBean("reloadSourceBean");
+        String aEnMessage = reMessage.getMessage("one", params, Locale.US);
+        System.out.println("刷新前的信息" + aEnMessage);
+        Thread.sleep(100000);
+        String bEnMessage = reMessage.getMessage("one", params, Locale.US);
+        System.out.println("刷新后的信息" + bEnMessage);*/
 
-
-
-
-
-
+        //通过容器启动时的initMessage()方法直接通过ApplicationContext加载国际化的信息资源
+        String internetMessageInfo=ctx.getMessage("one",params,Locale.CANADA);
+        System.out.println("通过容器启动时的initMessage()方法直接通过ApplicationContext加载国际化的信息资源:"+internetMessageInfo);
 
 
 
