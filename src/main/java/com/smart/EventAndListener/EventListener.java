41a79fbee75f6904c4057670795672256b3012d7
pengrenjun <809507790@qq.com>
2018-03-30 09:39:24 +0800

    容器事件的监听器与控制器的配置与使用

---
+++ src/main/java/com/smart/EventAndListener/EventListener.java
@@ -0,0 +1,23 @@
+package com.smart.EventAndListener;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+/*事件监听器 需要继承ApplicationListener*/
+public class EventListener  implements ApplicationListener<ApplicationEvent> {
+
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+
+       if(event.getClass().equals(MailSentEvent.class)){
+
+               MailSentEvent mailSentEvent=(MailSentEvent)event;
+               System.out.println("监听器监听到邮件发送"+"地址："+mailSentEvent.getAddtress()+" 收件人:"+mailSentEvent.getReceiver());
+       }
+    }
+
+
+    public static class SpringAopTest {
+    }
+}
---
+++ src/main/java/com/smart/EventAndListener/TestEventAndListener.java
@@ -0,0 +1,16 @@
+package com.smart.EventAndListener;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/*测试邮件发送监听*/
+public class TestEventAndListener {
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx=new ClassPathXmlApplicationContext("beans.xml");
+        EventSender eventSender=ctx.getBean("eventSender",EventSender.class);
+        MailSentEvent mail=new MailSentEvent(ctx,"沈阳市","xiaoli");
+        eventSender.sendMail(mail);
+    }
+}
